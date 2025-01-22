eeadd93a61e17b1e7520cfe64f39640e85c89186
novasilvosa <152951033+novasilvosa@users.noreply.github.com>
2025-01-22 12:09:15 +0800

    Add files via upload

---
+++ PracticalExam_silvosa/script.js
@@ -0,0 +1,7 @@
+// Existing code: Console log to check if JavaScript is working
+console.log("JavaScript is working!");
+
+// New code: Show an alert when the button is clicked
+document.getElementById("alertButton").addEventListener("click", function() {
+    alert("Hello, welcome to my webpage!");
+});
